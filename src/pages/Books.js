4ac61bc217ebf3307458dd895ed0b282df64b972
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-08-26 15:46:02 +0100

    update redux to work with react

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Element from '../components/Element';
 import FormToAdd from '../components/FormToAdd';
 
 const Books = () => {
-  const books = [
-    { id: 1, title: 'Influence & manipulation', author: 'Robert Cialdini' },
-    { id: 2, title: '7 secrets of warren buffett', author: 'Mary Buffett' },
-  ];
+  const books = useSelector((state) => state.books);
 
   return (
     <>
       {books.map((book) => (
-        <Element key={book.id} title={book.title} author={book.author} />
+        <Element key={book.id} id={book.id} title={book.title} author={book.author} />
       ))}
       <FormToAdd />
     </>
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,20 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    title: 'Influence & Manipulation',
+    author: 'Robert Cialdini',
+    id: uuidv4(),
+  },
+  {
+    title: '7 secrets of warren buffett',
+    author: 'Mary buffett',
+    id: uuidv4(),
+  },
+];
 
 const reducerForAddAndRemove = (state = initialState, action) => {
   switch (action.type) {
@@ -11,7 +24,7 @@ const reducerForAddAndRemove = (state = initialState, action) => {
         action.book,
       ];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.index);
+      return state.filter((book) => book.id !== action.book);
     default:
       return state;
   }
